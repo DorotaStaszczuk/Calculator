c8f87345484df68e6dda6a40dd7527ebce58024c
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2020-04-28 17:43:56 +0200

    displaying digits and operators working

--- calc.js
+++ calc.js
@@ -1,8 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
+    let result = 0;
+    let operator;
+    let firstOperand;
+    let secondOperand;
+
     const clear = document.querySelector('.clear');
     const display = document.querySelector('.display');
 
     clear.addEventListener('click', function () {
         display.innerText = 0;
+        console.log('clearing the display');
+    });
+
+    const container = document.querySelector('.container');
+    container.addEventListener('click', function (e) {
+        if (e.target.matches('.digit')) {
+            console.log(e.target.innerText);
+            display.innerText = e.target.innerText;
+        } else if (e.target.matches('.operator')) {
+            console.log(e.target.innerText);
+            display.innerText = e.target.innerText;
+        }
     });
 });
